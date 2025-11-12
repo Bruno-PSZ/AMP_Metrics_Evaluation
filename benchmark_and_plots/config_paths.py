9387a454fffb6c2c15bc2239186f3c2ffa5a30de
Bruno-PSZ <117539430+Bruno-PSZ@users.noreply.github.com>
2025-11-12 19:10:05 +0100

    Add benchmark and plot notebooks

---
+++ benchmark_and_plots/config_paths.py
@@ -0,0 +1,36 @@
+PATHS_FOR_CONTROL = {
+    "positives": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_positives_MAX40.fasta",
+    "positives_hq": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_positives_hq_MAX40.fasta",
+    "negatives": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_negatives_MAX40.fasta",
+    "negatives_hq": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_negatives_hq_MAX40.fasta",
+    "random_uniform": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/random_amp_uniform_distribution_MAX40.fasta",
+    "random_standard": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/random_amp_with_standard_distribution_MAX40.fasta",
+    "UniProt": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/uniprot_8_50_100_50K_MAX40.fasta",
+}
+
+PATHS_FOR_MODELS = {
+    "AMP-Diffusion": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp-diffusion_MAX40.fasta",
+    "AMP-GAN": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp-gan_MAX40_50K.fasta",
+    "CPL-Diff": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/cpl-diff_MAX40.fasta",
+    "HydrAMP": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/hydramp_MAX40.fasta",
+    "OmegAMP": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/omegamp_MAX40_50K.fasta",
+    "AMP-LM": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_lm_MAX40.fasta",
+    "AMP-Muller": "/raid/brunopsz/Metrics_Eval/FINAL_MAX_40AA_AMP/amp_muller_MAX40.fasta",
+}
+
+PATHS_FOR_DISCRIMINATIVE_MODELS = {
+    "positives": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_positives_MAX40/battleamp_predictions.tsv",
+    "positives_hq": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_positives_hq_MAX40/battleamp_predictions.tsv",
+    "negatives": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_negatives_MAX40/battleamp_predictions.tsv",
+    "negatives_hq": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_negatives_hq_MAX40/battleamp_predictions.tsv",
+    "random_uniform": "/raid/brunopsz/Discriminatory_model_results/results_final/random_amp_uniform_distribution_MAX40/battleamp_predictions.tsv",
+    "random_standard": "/raid/brunopsz/Discriminatory_model_results/results_final/random_amp_with_standard_distribution_MAX40/battleamp_predictions.tsv",
+    "UniProt": "/raid/brunopsz/Discriminatory_model_results/results_final/uniprot_8_50_100_50K_MAX40/battleamp_predictions.tsv",
+    "HydrAMP": "/raid/brunopsz/Discriminatory_model_results/results_final/hydramp_MAX40/battleamp_predictions.tsv",
+    "AMP-GAN": "/raid/brunopsz/Discriminatory_model_results/results_final/amp-gan_MAX40_50K/battleamp_predictions.tsv",
+    "AMP-Diffusion": "/raid/brunopsz/Discriminatory_model_results/results_final/amp-diffusion_MAX40/battleamp_predictions.tsv",
+    "CPL-Diff": "/raid/brunopsz/Discriminatory_model_results/results_final/cpl-diff_MAX40/battleamp_predictions.tsv",
+    "OmegAMP": "/raid/brunopsz/Discriminatory_model_results/results_final/omegamp_MAX40_50K/battleamp_predictions.tsv",
+    "AMP-LM": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_lm_MAX40/battleamp_predictions.tsv",
+    "AMP-Muller": "/raid/brunopsz/Discriminatory_model_results/results_final/amp_muller_MAX40/battleamp_predictions.tsv",
+}
